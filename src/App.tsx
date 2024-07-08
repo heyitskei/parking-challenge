86b8f19a23deca2c666a81b5bb33194ca1df52e0
e.stroganova <e.stroganova@vehikl.com>
2024-07-08 16:16:58 -0400

    feat: introduce conditional rendering for checkin/checkout screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,40 @@
 import './App.css'
+import {useState} from "react";
 
 function App() {
+    const [screen, setScreen] = useState('welcome');
+    const displayCheckInScreen = () => {
+        setScreen('checkin');
+    }
 
-  return (
+    const displayCheckOutScreen = () => {
+        setScreen('checkout');
+    }
+
+    return (
       <>
           <h1>Welcome</h1>
           <div className='App'>
-              <button>In</button>
-              <button>Out</button>
+              <button className='checkInButton' onClick={displayCheckInScreen}>In</button>
+              <button className='checkOutButton' onClick={displayCheckOutScreen}>Out</button>
           </div>
-          <div>
-              <input type='text' placeholder='Licence plate #'/>
-          </div>
-          <button type='submit'>Submit</button>
+          {screen === 'checkin' ? (
+          <div className='CheckIn'>
+          <input type='text' placeholder='Enter licence plate number' />
+              <div>
+                  <button type='button' onClick={displayCheckInScreen}>Check In</button>
+              </div>
+          </div>) :
+              screen === 'checkout' ? (<div className='CheckOut'>
+                  <input type='text' placeholder='Enter licence plate number'/>
+              <div>
+                  <button type='button' onClick={displayCheckInScreen}>Calculate rate</button>
+                  <button type='button' onClick={displayCheckInScreen}>Process</button>
+              </div>
+          </div>) : <div></div>
+          }
       </>
-  )
+    )
 }
 
 export default App
