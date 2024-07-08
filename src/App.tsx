0d40806b9b2b18487e408701d9db3debdc1b18d7
e.stroganova <e.stroganova@vehikl.com>
2024-07-08 15:51:15 -0400

    feat: initial commit/first draft

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+      <>
+          <h1>Welcome</h1>
+          <div className='App'>
+              <button>In</button>
+              <button>Out</button>
+          </div>
+          <div>
+              <input type='text' placeholder='Licence plate #'/>
+          </div>
+          <button type='submit'>Submit</button>
+      </>
+  )
+}
+
+export default App
