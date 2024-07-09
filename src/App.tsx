fc82beefffbd3c668c432941f8d395258f14b614
e.stroganova <e.stroganova@vehikl.com>
2024-07-09 15:51:29 -0400

    feat: notify that user the max limit was exceeded

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
     const [screen, setScreen] = useState('welcome');
     const [licencePlateNumber, setLicencePlateNumber] = useState('');
     const [garage, setGarage] = useState<string[]>([]);
+    const [isGarageFull, setIsGarageFull] = useState(false);
     const capacity = 3;
     const displayCheckInScreen = () => {
         setScreen('checkin');
@@ -25,6 +26,11 @@ function App() {
             setGarage([...garage, licencePlateNumber]);
         }
 
+        if (garage.length === capacity)
+        {
+            setIsGarageFull(true);
+        }
+
         localStorage.setItem('licencePlateNumbers', JSON.stringify(garage));
     }
 
@@ -39,20 +45,20 @@ function App() {
               <button className='checkInButton' onClick={displayCheckInScreen}>In</button>
               <button className='checkOutButton' onClick={displayCheckOutScreen}>Out</button>
           </div>
-          {screen === 'checkin' ? (
-          <div className='CheckIn'>
-          <input type='text' placeholder='Enter licence plate number' value={licencePlateNumber} onChange={processLicencePlateCheckIn} />
+          {screen === 'checkin' ? <div className='CheckIn'>
+          <input type='text' placeholder='Enter licence plate #' value={licencePlateNumber} onChange={processLicencePlateCheckIn} />
               <div>
+                  { isGarageFull ? <div>GARAGE IS FULL</div> : ''}
                   <button type='button' onClick={registerCar}>Check In</button>
               </div>
-          </div>) :
-              screen === 'checkout' ? (<div className='CheckOut'>
-                  <input type='text' placeholder='Enter licence plate number' value={licencePlateNumber} onChange={processLicencePlateCheckIn}/>
+          </div> :
+              screen === 'checkout' ? <div className='CheckOut'>
+                  <input type='text' placeholder='Enter licence plate #' value={licencePlateNumber} onChange={processLicencePlateCheckIn}/>
                   <div>
                       <button type='button' onClick={displayCheckInScreen}>Calculate rate</button>
                       <button type='button' onClick={displayCheckInScreen}>Process</button>
                   </div>
-              </div>) : <div></div>
+              </div> : <div></div>
           }
       </>
     )
